51f75266a5462ebeb7f7f8dee195ee0d1012a8af
Andras Szabo <andras.szabo@pivigo.com>
2019-10-30 13:43:48 +0000

    Add probabilities for typo and abbreviation

--- main.go
+++ main.go
@@ -123,19 +123,29 @@ func fuzzRecord(record Record, resultChan chan Record, wg *sync.WaitGroup) {
 	defer wg.Done()
 	newRecord := record.record
 	// determine which function classes we should run
-	shouldTypo := rand.Intn(2)
+	//shouldTypo := rand.Intn(100000)
+	//shouldTypo := 0
+	shouldTypo := rand.Float64()
+	probTypo := 0.01
 
-	shouldAbbreviate := rand.Intn(2)
+	//shouldAbbreviate := rand.Intn(100000)
+	//shouldAbbreviate := 0
+	shouldAbbreviate := rand.Float64()
+	probAbbrev := 0.001
 	// determine which columns we should fuzz
 	shouldFuzzColumns := []int{}
 	for range newRecord {
 		shouldFuzzColumns = append(shouldFuzzColumns, rand.Intn(2))
+		//shouldFuzzColumns = append(shouldFuzzColumns, 0)
+		//shouldFuzzColumns = append(shouldFuzzColumns, rand.Float64())
 	}
 	// TODO: splitting it up like this is actually quite inefficient...
-	if shouldTypo == 1 {
+	//if shouldTypo == 1 {
+	if shouldTypo <= probTypo {
 		newRecord = generateTypo(newRecord, shouldFuzzColumns)
 	}
-	if shouldAbbreviate == 1 {
+	//if shouldAbbreviate == 1 {
+	if shouldAbbreviate <= probAbbrev {
 		newRecord = generateAbbreviations(newRecord, shouldFuzzColumns)
 	}
 	resultChan <- Record{
